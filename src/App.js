db8bf3e3aef75405532015333fd5114f1185a097
vincent <innocent0705@gmail.com>
2023-05-23 15:17:29 +0800

    added sign-in component and firebase utils and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import Directory from './components/directory/directory.componrnt';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const App = () => {
 
-  const categories = [
-    {
-      id: 1,
-      title: 'Climbing Equiment',
-      imageUrl: 'https://images.unsplash.com/photo-1569783169977-8c84482eb0ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
-    },
-    {
-      id: 2,
-      title: 'Camping / Trekking',
-      imageUrl: 'https://images.unsplash.com/photo-1601134917279-ef70a0a90f18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
-    },
-    {
-      id: 3,
-      title: 'Training',
-      imageUrl: 'https://images.unsplash.com/photo-1593234270120-189e191918a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'
-    },
-    {
-      id: 4,
-      title: 'Clothing',
-      imageUrl: 'https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
-    },
-    {
-      id: 5,
-      title: 'Footrwear',
-      imageUrl: 'https://plus.unsplash.com/premium_photo-1672280913342-06cfe011bc62?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1658&q=80'
-    }
-  ];
+  const Shop = () => {
+    return (
+      <h1> I am the shop page</h1>
+    )
+  };
+
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/utils/firebase.utils.js
@@ -0,0 +1,51 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBocEJnPYjtXM0_O0Oqwm6jLBOwLAFjHhg",
+    authDomain: "ecommerce-9d464.firebaseapp.com",
+    projectId: "ecommerce-9d464",
+    storageBucket: "ecommerce-9d464.appspot.com",
+    messagingSenderId: "528471035767",
+    appId: "1:528471035767:web:4bee5ffd92504ece5b9b72"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
